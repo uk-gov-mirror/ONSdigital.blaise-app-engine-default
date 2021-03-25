b4c4b3912ac47478bab5dfc0490e0a0e5e729e72
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-25 17:15:45 +0000

    Homepage

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let {GOOGLE_CLOUD_PROJECT} = process.env;
 
 const TOBI_URL = `https://tobi-ui-dot-${GOOGLE_CLOUD_PROJECT}.nw.r.appspot.com`
 const DQS_URL = `https://dqs-ui-dot-${GOOGLE_CLOUD_PROJECT}.nw.r.appspot.com`
+const BUM_URL = `https://bum-ui-dot-${GOOGLE_CLOUD_PROJECT}.nw.r.appspot.com`
+const DDM_URL = `https://ddm-ui-dot-${GOOGLE_CLOUD_PROJECT}.nw.r.appspot.com`
 
 app.get("/404", async function (req, res) {
     console.log("Server called");
@@ -21,7 +23,7 @@ app.get("/500", async function (req, res) {
 
 app.get("*", async function (req, res) {
     console.log("Server called");
-    res.render("../views/index.html", {TOBI_URL, DQS_URL});
+    res.render("../views/index.html", {TOBI_URL, DQS_URL, BUM_URL, DDM_URL});
 });
 
 
